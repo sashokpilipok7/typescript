93ae49e118d33c3f9d72e4003de4187549505a71
SashaPilipenko__ <sashapylypenko.pov@gmail.com>
2020-12-06 22:53:57 +0200

    new build

---
+++ web/build/precache-manifest.1ebfee82fefdb73e2dcfc65468a4d8f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e597f6feeba20c6a9604b822fb78aca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ce2f738ba390d156632",
+    "url": "/static/css/2.1a94ea9b.chunk.css"
+  },
+  {
+    "revision": "92c0231d510d21b9bcc5",
+    "url": "/static/css/main.792b417c.chunk.css"
+  },
+  {
+    "revision": "3ce2f738ba390d156632",
+    "url": "/static/js/2.ab239d39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab239d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92c0231d510d21b9bcc5",
+    "url": "/static/js/main.836e394c.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
